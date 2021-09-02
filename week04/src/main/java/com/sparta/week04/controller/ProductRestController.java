d750cd77df82635b9cfa0137da11fc040dfd7828
jeonho <jhkang1995@naver.com>
2021-09-02 16:45:56 +0900

    네이버 쇼핑 API사용

---
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -0,0 +1,35 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.models.Product;
+import com.sparta.week04.models.ProductMypriceRequestDto;
+import com.sparta.week04.models.ProductRepository;
+import com.sparta.week04.models.ProductRequestDto;
+import com.sparta.week04.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductRestController {
+
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+    @GetMapping("/api/products")
+    public List<Product> readProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+
+        return productService.update(id, requestDto);
+    }
+}
